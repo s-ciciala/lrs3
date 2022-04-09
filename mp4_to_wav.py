da887084539c18880ef7c8197ed249743db5a753
SzymonCiciala <47600083+s-ciciala@users.noreply.github.com>
2022-04-09 02:33:36 +0100

    mp4 to wav

---
+++ mp4_to_wav.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import read, write
+from IPython.display import Audio
+import moviepy.editor as mp
+import librosa
+import os
+import sys
+from pydub import AudioSegment
+
+SPLITS = ["test/","train/","val/"]
+PATH_TO_LRS3 = "/disk/scratch2/s1834237/LRS3_wav/LRS3/"
+
+def convert_mp4(mp4_path, output_file):
+    '''
+    reads the mp4 file and wrties to desired format
+    :param mp4_path (string): needs to be in form -r"Shrek.mp4"- as r indicates read
+    :param output_file (string): needs to be in form -r"Shrek.wav"- as r indicates read
+    :return None: writes file
+    '''
+    video = mp.VideoFileClip(mp4_path)  # here sample.mp4 is the name of video clip. 'r' indicates that we are
+    video.audio.write_audiofile(output_file)  # Here output_file is the name of the audio file with type e.g "Shrek.wav"
+
+    return video.audio.to_soundarray()
+
+for split in SPLITS:
+    dataset_path = PATH_TO_LRS3 + split
+    files = os.listdir(dataset_path)
+    for file in files:
+        file_path = dataset_path + file +"/"
+        examples = os.listdir(file_path)
+        for example in examples:
+            if ".mp4" in example:
+                mp4_path = file_path + example
+                wav_form = example.split(".")
+                audio_file = wav_form[0] + ".wav"
+                wav_path = file_path + audio_file
+                convert_mp4(mp4_path,wav_path)
+
